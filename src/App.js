8b3d575423260970bb332b0225c38a6246cbd6af
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2022-12-20 20:13:13 -0600

    Creating navbar and routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import IndexPage from './Pages/IndexPage';
 
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path='/' element={<IndexPage />} />
+      </Routes>
     </div>
   );
 }
