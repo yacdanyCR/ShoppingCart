c50caa7d684e4b9a025a6429ff094d45458987fd
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2022-12-20 20:14:11 -0600

    Adding navbar

---
+++ src/Pages/IndexPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import NavBarComponent from '../Components/NavBarComponent/NavBarComponent'
+
+export const IndexPage = () => {
+    return (
+        <>
+            <header>
+                <NavBarComponent />
+            </header>
+            <main>
+
+            </main>
+            <footer>
+
+            </footer>
+        </>
+    )
+}
+
+export default IndexPage
