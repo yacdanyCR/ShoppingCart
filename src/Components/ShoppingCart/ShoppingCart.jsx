5521f2a18bd55b908623897ee138cbf956637708
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2022-12-22 15:13:56 -0600

    Removing elements from the shopping cart

--- src/Components/ShoppingCart/ShoppingCart.jsx
+++ src/Components/ShoppingCart/ShoppingCart.jsx
@@ -1,20 +1,26 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { BsFillCartFill } from "react-icons/bs";
 import { Button, Col, Offcanvas, Row } from 'react-bootstrap'
 import { CartContext } from '../../Context/CartContext';
+import { VscChromeClose } from "react-icons/vsc";
 
 export const ShoppingCart = () => {
-    const { shoppingCartQuantity, cart } = useContext(CartContext);
+    const { shoppingCartQuantity, shoppingCartAllPrice, removeProductFromCart, cart } = useContext(CartContext);
 
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
+    const handleDelete = (product) => {
+        removeProductFromCart(product.id);
+        setShow(false)
+    }
+
     return (
         <>
             <Button variant="primary" onClick={handleShow}>
-                <BsFillCartFill />
+                <BsFillCartFill size={20} />
             </Button>
 
             <div style={{ position: 'relative', right: '1rem', top: '1rem', padding: '2px', width: '20px', textAlign: 'center', borderRadius: '20px', background: 'red' }}>
@@ -29,13 +35,26 @@ export const ShoppingCart = () => {
                     <Row>
                         {cart.map((el) => {
                             return (
-                                <Col sm={12}>
-                                    <h1>{el.title}</h1>
-                                    <img src={el.img} alt="img_Product" />
+                                <Col sm={12} style={{ marginBottom: '1rem' }}>
+                                    <div className="delete__ShoppingCart" style={{ position: 'relative', left: '85%', fontSize: '2rem' }}>
+                                        <VscChromeClose onClick={() => handleDelete(el)} size={35} color={'red'} style={{ cursor: 'pointer' }} />
+                                    </div>
+                                    <div className='shopping__Information'>
+                                        <h3>{el.title}</h3>
+                                        <img src={el.img} alt="img_Product" style={{ width: '100px' }} />
+                                    </div>
+
+                                    <div className='shopping__Operations'>
+                                        <Button>+</Button>
+                                        <input type='number' defaultValue={el.quantity}></input>
+                                        <Button>-</Button>
+
+                                    </div>
                                 </Col>
                             )
                         })}
                     </Row>
+                    <h1>{shoppingCartAllPrice()}$</h1>
                 </Offcanvas.Body>
             </Offcanvas>
         </>
--- src/Context/CartContext.js
+++ src/Context/CartContext.js
@@ -7,17 +7,40 @@ export const CartProvider = ({ children }) => {
     const [cart, setCart] = useState([])
 
     const addToShoppingCart = (data) => {
-        setCart((prev) => {
-            return [...prev, data]
-        })
+        const productObj = Object.assign({ quantity: 1 }, data);
+        const productExist = cart.filter((el) => el.id === productObj.id).length > 0 ? true : false;
+
+        if (!productExist) {
+            setCart((prev) => {
+                return [...prev, productObj]
+            })
+        } else {
+            const index = cart.findIndex((el) => el.id === productObj.id);
+            cart[index].quantity += 1;
+        }
+
     }
 
     const shoppingCartQuantity = () => {
         return cart.length
     }
 
+    const shoppingCartAllPrice = () => {
+        if (cart.length > 0) {
+            const quantities = cart.map((el) => el.quantity * el.price);
+            return quantities.reduce((prev, curr) => prev + curr).toFixed(2);
+        }
+        return 0
+    }
+
+    const removeProductFromCart = (id) => {
+        const prodictToRemove = cart.findIndex((el) => el.id === id);
+        cart.splice(prodictToRemove, 1);
+        setCart(cart);
+    }
+
     return (
-        <CartContext.Provider value={{ addToShoppingCart, shoppingCartQuantity, cart }}>{children}</CartContext.Provider>
+        <CartContext.Provider value={{ addToShoppingCart, shoppingCartQuantity, shoppingCartAllPrice, removeProductFromCart, cart, setCart }}>{children}</CartContext.Provider>
     )
 }
 
